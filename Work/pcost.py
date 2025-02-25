50dbdea7c20120bb613904e82e4c32bcabe98b5b
doosrasingh <doosrasingh@outlook.com>
2025-02-25 17:51:27 +0100

    Finish section 2

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,10 +1,27 @@
 # pcost.py
 #
 # Exercise 1.27
-with open('Data/portfolio.csv', 'rt') as f:
-    headers = next(f)
-    cost = 0
-    for line in f:
-        row = line.split(',')
-        cost += int(row[1]) * float(row[2])
-    print('Total cost', cost)
+
+import csv
+import sys
+
+def portfolio_cost(filename):
+    cost = 0.0
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)        
+        for rowno, row in enumerate(rows, start=1):
+            record = dict(zip(headers,row))
+            try:
+                cost += int(record['shares']) * float(record['price'])
+            except ValueError:
+                print(f'Row {rowno}: Couldn\'t convert:', row)
+    return cost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = input('Enter a filename:')
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,86 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+import sys
+
+def read_portfolio(filename):
+    '''
+    Opens a given portfolio file and reads it into a list of tuples.
+    '''
+    portfolio = []
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            record = dict(zip(headers, row))
+            holding = {
+                'name': record['name'],
+                'shares': int(record['shares']),
+                'price': float(record['price'])
+            }
+            portfolio.append(holding)
+    return portfolio
+
+def read_prices(filename):
+    '''
+    Reads a set of prices into a dictionary where the keys of the dictionary are the stock names and the values in the dictionary are the stock prices.
+    '''
+    prices = {}
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                pass
+    return prices
+
+
+def make_report(portfolio, prices):
+    '''
+    Make a list of (name, shares, price, change) tuples given a portfolio list
+    and prices dictionary.
+    '''
+    rows = []
+    for stock in portfolio:
+        current_price = prices[stock['name']]
+        change = current_price - stock['price']
+        summary = (stock['name'], stock['shares'], current_price, change)
+        rows.append(summary)
+    return rows
+
+
+# Read data files for portfolio and prices
+portfolio = read_portfolio('Data/portfolio.csv')
+prices = read_prices('Data/prices.csv')
+
+
+# Generate report data
+report = make_report(portfolio, prices)
+
+# Output the report
+headers = ('Name', 'Shares', 'Price', 'Change')
+print('%10s %10s %10s %10s' % headers)
+print(('-'*10 + ' ') * len(headers))
+for name, shares, price, change in report:
+    dollar_price = f'${price:0.2f}'
+    print(f'{name:>10s} {shares:>10d} {dollar_price:>10s} {change:>10.2f}')
+    
+
+# Calculate the total cost of the portfolio
+total_cost = 0.0
+for share in portfolio:
+    total_cost += share['shares'] * share['price']
+
+print('Total cost', f'{total_cost:0.2f}')
+
+# Calculate the current value of the portfolio
+current_value = 0.0
+for share in portfolio:
+    current_value += share['shares'] * prices[share['name']]
+
+print('Current value', f'{current_value:0.2f}')
+gain = current_value - total_cost
+print('Current gain', f'{gain:0.2f}')
